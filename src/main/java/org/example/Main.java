55838e9cdc33ba037aca33cc69cf425c927d69a1
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-10 08:47:59 +0100

    no update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,7 @@ public class Main {
     static void main(String[] args) {
 
         System.out.println("Erster SChritt");
-        System.out.println("2 Schritt");
-        System.out.println("hallilooo");
-        System.out.println("bin master 1");
-        System.out.println("GoNacht");
+
 
 
 
