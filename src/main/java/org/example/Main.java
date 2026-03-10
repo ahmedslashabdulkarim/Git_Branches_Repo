0a7d59b90789d39bf30c6b5cf544aca8c15e3329
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-10 08:33:49 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    static void main(String[] args) {
+
+        System.out.println("Erster SChritt");
+        System.out.println("2 Schritt");
+        System.out.println("hallilooo");
+        System.out.println("bin master 1");
+        System.out.println("GoNacht");
+
+
+
+
+    }
+}
