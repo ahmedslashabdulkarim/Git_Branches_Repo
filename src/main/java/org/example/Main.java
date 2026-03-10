7d3e2e6011c77af1691a112d9778e345c1ae76c6
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-10 08:54:47 +0100

    add 1 line von test-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         System.out.println("Erster SChritt");
         System.out.println("Hello World");
-        System.out.println("Hello von test-1");
+        System.out.println("Hello von test-1 ");
 
         
 
