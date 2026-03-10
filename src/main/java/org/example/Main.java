bbf49503825a484bced27417decbabd1fc0349cd
ahmed <ahmedslashabdulkarim@gmail.com>
2026-03-10 08:59:13 +0100

    add 1 line von test-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     static void main(String[] args) {
 
         System.out.println("Erster SChritt");
+        System.out.println("Hello");
+        System.out.println(" World");
 
 
 
